2f90b9daa523da6c2fc1fda8a992b64050181e63
roxeman3 <roxeman3@gmail.com>
2014-07-01 18:25:45 +0200

    Initial Commit

---
+++ MalometerTDDWeek5/IHAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  IHAppDelegate.h
+//  MalometerTDDWeek5
+//
+//  Created by Carlos Roig Salvador on 01/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MalometerTDDWeek5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MalometerTDDWeek5
+//
+//  Created by Carlos Roig Salvador on 01/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IHAppDelegate class]));
+    }
+}
