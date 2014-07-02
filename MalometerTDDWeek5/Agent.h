28a9eacf8c9b6922ac8df4d7019ade54a130fb7b
roxeman3 <roxeman3@gmail.com>
2014-07-02 09:48:07 +0200

    First Commit

---
+++ MalometerTDDWeek5/Agent.h
@@ -0,0 +1,17 @@
+//
+//  Agent.h
+//  MalometerTDDWeek5
+//
+//  Created by Carlos Roig Salvador on 02/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Agent : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ MalometerTDDWeek5/Agent.m
@@ -0,0 +1,16 @@
+//
+//  Agent.m
+//  MalometerTDDWeek5
+//
+//  Created by Carlos Roig Salvador on 02/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import "Agent.h"
+
+
+@implementation Agent
+
+@dynamic name;
+
+@end
